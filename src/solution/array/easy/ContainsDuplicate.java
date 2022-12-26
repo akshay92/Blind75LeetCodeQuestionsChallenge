f6f8066293d7a9d0258d6af668343adaef1d1fbd
akshay <akshaytrivedi92@gmail.com>
2022-12-26 16:43:28 +0530

    ContainsDuplicate(Sort and check first and second pointer )

---
+++ src/solution/array/easy/ContainsDuplicate.java
@@ -0,0 +1,39 @@
+package solution.array.easy;
+
+import java.util.Arrays;
+
+/*
+https://leetcode.com/problems/contains-duplicate/
+1. Sort
+2. Run loop and check first second pointer value
+ */
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        ContainsDuplicate object = new ContainsDuplicate();
+    }
+
+    /*
+    Example 1:
+   Input: nums = [1,2,3,1]
+   Output: true
+   Example 2:
+
+   Input: nums = [1,2,3,4]
+   Output: false
+   Example 3:
+
+   Input: nums = [1,1,1,3,3,4,3,2,4,2]
+   Output: true
+     */
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int ind = 1; ind < nums.length; ind++) {
+            if(nums[ind] == nums[ind - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}

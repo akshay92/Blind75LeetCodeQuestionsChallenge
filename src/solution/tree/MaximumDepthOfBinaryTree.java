9410982cbb38b3bbc3bbcfc5fc0e14b2b7ef1ca4
akshay <akshaytrivedi92@gmail.com>
2023-01-17 12:40:10 +0530

    MaximumDepthOfBinaryTree(Recursion)

---
+++ src/solution/tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,16 @@
+package solution.tree;
+
+import solution.common.TreeNode;
+
+public class MaximumDepthOfBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+
+    }
+}
